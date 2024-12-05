1cac3aece552a11062b4b49406b54baa14aa918f
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2024-12-05 14:23:11 +0700

    feat: dummy use variable

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -13,7 +13,7 @@ function Timer({
   return (
     <div className="w-4/5 max-w-2xl p-4 mx-auto rounded border-2">
       <div className="w-full text-center text-white mb-4 p-2 rounded text-2xl" style={{ backgroundColor: color}}>
-        <h2>{title}</h2>
+        <h2>{title} ({ interval })</h2>
       </div>
 
       <div className="w-full text-center text-white mb-4 p-2 rounded text-9xl" style={{ backgroundColor: color}}>
